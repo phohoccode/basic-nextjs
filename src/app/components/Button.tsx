48876ee7207de0d5e9640c709d07a8b7ccde482c
Nhan Quốc Việt <qviet092@gmail.com>
2024-12-18 00:14:20 +0700

    khởi tạo dự án

---
+++ src/app/components/Button.tsx
@@ -0,0 +1,7 @@
+const Button = () => {
+    return ( 
+        <button>Xin chào!</button>
+     );
+}
+ 
+export default Button;
---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import Button from "./components/Button";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <Button />
+    </div>
+  );
+}
